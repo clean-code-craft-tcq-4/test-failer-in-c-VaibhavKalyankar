f3c8dd9840d5cc5b793982878b71c192621fb1a8
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-06-22 18:23:59 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -18,6 +18,7 @@ int main() {
     assert(size(38) == 'S');
     assert(size(40) == 'M');
     assert(size(42) == 'M');
+    assert(size(42) == 'L');
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
