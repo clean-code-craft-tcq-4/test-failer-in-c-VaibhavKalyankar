3e2bea9681ea6058b85bf29a12f3f82acaa28b20
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-07-02 23:58:02 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <assert.h>
+#include <alterter.h>
 
 int alertFailureCount = 0;
 void TestAlertFailureCount();
 
 int networkAlertStub(float celcius) {
+    
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    if(celcius <= 100.00f)
-        return 200;
-    else if(celcius > 100.00f)
-        return 500;
+    if(celcius <= THRESHOLD_CELCIUS)
+        return OK;
     else
-        return 200;
+        return NOT_OK
+}
+
+#if(PRODUCTION_ENABLED != PRODUCTION_MODE_ON)
+void TesteralertinCelcius(float Celcius,int ReturnCode)
+{
+    assert(Celcius<ReturnCode);
 }
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
+#if(PRODUCTION_ENABLED == PRODUCTION_MODE_ON)
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -24,6 +31,9 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount++;
     }
+#else
+    TesteralertinCelcius(celcius,returnCode);
+#endif
 }
 
 void TestAlertFailureCount()
@@ -38,7 +48,9 @@ void TestAlertFailureCount()
 
 int main() {
     alertInCelcius(400.5);
+    assert(alertFailureCount +1 == 1)
     alertInCelcius(303.6);
+    assert(alertFailureCount == 0)
     TestAlertFailureCount();
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
