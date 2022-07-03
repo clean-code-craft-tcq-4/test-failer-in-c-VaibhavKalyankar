b17ba537d830f7e55a3e0facbd6be1b382b59d95
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-07-03 18:05:01 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <assert.h>
+#include <misaligned.h>
+
 enum MajorColor {WHITE,RED,BLACK,YELLOW,VIOLET};
 enum MinorColor {BLUE,ORANGE,GREEN,BROWN,SLATE};
 int GetPairNumberFromColor(MajorColor, MinorColor);
 void TestPairtoNumber(MajorColor, MinorColor,int);
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int NumberofMajorColors = sizeof(majorColor) / sizeof(majorColor[0]);
+int NumberofMinorColors = sizeof(minorColor) / sizeof(minorColor[0]);
+
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+    int MajorColorCnt,MinorColorCnt=0;
+    ColorPair ColorPairRef;
+    printf("Reference Manual\n");
+   
+    for(MajorColorCnt = 0; MajorColorCnt < NumberofMajorColors; MajorColorCnt++) {
+        for(MinorColorCnt = 0; MinorColorCnt < NumberofMinorColors; MinorColorCnt++) {
+            ColorPairRef.majorColor = (MajorColor)MajorColorCnt;
+            ColorPairRef.minorColor = (MinorColor)MinorColorCnt;
+            int PairNumber =  GetPairNumberFromColor(&ColorPairRef);
+            
+            printf("%d | %s | %s\n", PairNumber, majorColor[MajorColorCnt], minorColor[MinorColorCnt]);
         }
     }
-    return i * j;
+    return MajorColorCnt * MinorColorCnt;
 }
 
-int GetPairNumberFromColor(MajorColor Majorcolor, MinorColor Minorcolor)
+int GetPairNumberFromColor(const ColorPair *colorPair)
 {
-    return Majorcolor * 5 + Minorcolor + 1;
+    return colorPair->majorColor * NumberofMinorColors + colorPair->majorColor + 1;
 }
 
 void TestPairtoNumber(MajorColor Majorcolor, MinorColor Minorcolor,int ExpPairNumber)
 {
+    ColorPair colorpair;
+    colorpair.majorColor = Majorcolor;
+    colorpair.minorColor = Minorcolor;
     int PairNumber;
     PairNumber = GetPairNumberFromColor(Majorcolor,Minorcolor);
     assert(PairNumber == ExpPairNumber);
